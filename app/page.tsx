e8bc43e44498987edf556d3d0180173a22373311
Alifta Maulana Rayhan <aliftamaulanarg@gmail.com>
2024-11-14 10:44:21 +0700

    add a next js project, with a abstract dashboard

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="h1">Lany Drive - the only storage solution you need.</h1>
+    </div>
+  );
+}
